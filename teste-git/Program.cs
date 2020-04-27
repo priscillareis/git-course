49408292b6f4ef969e58395aa820f08d64d89c4e
Priscilla Reis <priscillareissouza@outlook.com>
2020-04-27 19:58:36 -0300

    Inseri mensagem de boa noite

--- teste-git/Program.cs
+++ teste-git/Program.cs
@@ -4,6 +4,7 @@ namespace teste_git {
     class Program {
         static void Main(string[] args) {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Boa noite");
         }
     }
 }
